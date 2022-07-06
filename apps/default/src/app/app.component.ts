7893ba1741b7965f23865fa27d45015e31edc928
Joseba Andres <joseba.andres@crealogix.com>
2022-07-06 16:04:36 +0200

    initial commit

---
+++ apps/default/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'change-detection-strategy-examples-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'default';
+}
