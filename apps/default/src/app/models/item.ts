8767fa1fffab8f8a2b1f930e43c86487a1abec97
Joseba Andres <joseba.andres@crealogix.com>
2022-07-08 09:25:04 +0200

    add counts

--- apps/default/src/app/models/item.ts
+++ apps/default/src/app/models/item.ts
@@ -1,6 +1,37 @@
-export interface ItemModel {
+import { BehaviorSubject } from 'rxjs';
+
+export class ItemModel {
   id: string;
   code: string;
-  description: string;
+  private _description = '';
+  get description(): string {
+    this.addDescriptionGetCount();
+    return this._description;
+  }
+  set description(value: string) {
+    this._description = value;
+  }
+  componentConstructionCount = 0;
+  descriptionGetCount$ = new BehaviorSubject<number>(0);
   items?: Array<ItemModel>;
+
+  constructor(params: {
+    id: string;
+    code: string;
+    description: string;
+    items?: Array<ItemModel>;
+  }) {
+    this.id = params.id;
+    this.code = params.code;
+    this.description = params.description;
+    this.items = params.items;
+  }
+
+  addDescriptionGetCount() {
+    this.descriptionGetCount$.next(this.descriptionGetCount$.value + 1);
+  }
+
+  addComponentConstructionCount() {
+    this.componentConstructionCount = this.componentConstructionCount + 1;
+  }
 }
--- apps/default/src/app/services/data.service.ts
+++ apps/default/src/app/services/data.service.ts
@@ -4,31 +4,51 @@ import { ItemModel } from '../models/item';
 @Injectable({ providedIn: 'root' })
 export class DataService {
   data: Array<ItemModel> = [
-    {
+    new ItemModel({
       id: '1',
       code: 'Cod. 1',
       description: 'Item 1',
       items: [
-        { id: '1.1', code: 'Cod. 1.1', description: 'Item 1.1' },
-        { id: '1.2', code: 'Cod. 1.2', description: 'Item 1.2' },
+        new ItemModel({
+          id: '1.1',
+          code: 'Cod. 1.1',
+          description: 'Item 1.1',
+        }),
+        new ItemModel({
+          id: '1.2',
+          code: 'Cod. 1.2',
+          description: 'Item 1.2',
+        }),
       ],
-    },
-    {
+    }),
+    new ItemModel({
       id: '2',
       code: 'Cod. 2',
       description: 'Item 2',
       items: [
-        { id: '2.1', code: 'Cod. 2.1', description: 'Item 2.1' },
-        {
+        new ItemModel({
+          id: '2.1',
+          code: 'Cod. 2.1',
+          description: 'Item 2.1',
+        }),
+        new ItemModel({
           id: '2.2',
           code: 'Cod. 2.2',
           description: 'Item 2.2',
           items: [
-            { id: '2.2.1', code: 'Cod. 2.2.1', description: 'Item 2.2.1' },
-            { id: '2.2.2', code: 'Cod. 2.2.2', description: 'Item 2.2.2' },
+            new ItemModel({
+              id: '2.2.1',
+              code: 'Cod. 2.2.1',
+              description: 'Item 2.2.1',
+            }),
+            new ItemModel({
+              id: '2.2.2',
+              code: 'Cod. 2.2.2',
+              description: 'Item 2.2.2',
+            }),
           ],
-        },
+        }),
       ],
-    },
+    }),
   ];
 }
