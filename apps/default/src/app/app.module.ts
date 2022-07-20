1d663e46200c461e8da773d9343f075e975c2b2d
Joseba Andres <joseba.andres@crealogix.com>
2022-07-20 12:14:47 +0200

    add angular material

--- apps/default/src/app/app.module.ts
+++ apps/default/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeroComponent, HeroesComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
