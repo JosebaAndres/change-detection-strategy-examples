1782717d71ff1a153e831805ed42f521d6737b08
Joseba Andres <joseba.andres@crealogix.com>
2022-07-26 12:19:51 +0200

    feature/add-app-update-pet-on-push

--- apps/default/src/app/components/update-pet/update-pet.component.ts
+++ apps/default/src/app/components/update-pet/update-pet.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ViewChild,
+} from '@angular/core';
+import { Observable } from 'rxjs';
 import { PetModel } from '../../models/pet';
 import { PetsService } from '../../services/pets.service';
 import { ContextComponent } from '../context/context.component';
@@ -7,10 +13,10 @@ import { ContextComponent } from '../context/context.component';
   selector: 'app-update-pet',
   templateUrl: './update-pet.component.html',
   styleUrls: ['./update-pet.component.scss'],
-  changeDetection: ChangeDetectionStrategy.Default,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UpdatePetComponent {
-  selectedPet: PetModel | null = null;
+  selectedPet$: Observable<PetModel | null> = this.petsService.selectedPet$;
   selectedPetName: string | null | undefined = '';
 
   @ViewChild('appContext', { static: true }) appContext!: ContextComponent;
@@ -20,10 +26,13 @@ export class UpdatePetComponent {
     return 'Update pet component';
   }
 
-  constructor(private petsService: PetsService) {
+  constructor(
+    private petsService: PetsService,
+    private cdr: ChangeDetectorRef
+  ) {
     this.petsService.selectedPet$.subscribe((selectedPet) => {
-      this.selectedPet = selectedPet;
       this.selectedPetName = selectedPet?.name;
+      this.cdr.markForCheck();
     });
   }
 
